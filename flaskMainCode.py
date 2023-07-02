68d39ea783df880fab95ac484f8c769614bcd6d7
Yoru-cyber <carlosmendez170210@gmail.com>
2023-07-02 10:50:43 -0400

    actualización jinja templates

--- flaskMainCode.py
+++ flaskMainCode.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, url_for, request, jsonify
+from flask_bootstrap import Bootstrap
 
 urlAPI_Random = 'https://uselessfacts.jsph.pl/'
 app = Flask(__name__)
 @app.route("/", methods=['GET'])
 
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+if __name__ == "__main__":
+    app.run(debug=True)
 @app.route('/message', methods=['GET'])
 def apiCallTest():
     return jsonify({'message' : '''It's alive'''})
